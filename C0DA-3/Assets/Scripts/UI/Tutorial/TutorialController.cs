90d098418e63ca963eb403faf99f0408ca73a3ad
aike <aikesiero@gmail.com>
2026-04-08 23:38:23 +0200

    Tutorial Text Animation and Hide

--- C0DA-3/Assets/Scripts/UI/Tutorial/TutorialController.cs
+++ C0DA-3/Assets/Scripts/UI/Tutorial/TutorialController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using DG.Tweening;
 
 public class TutorialController : MonoBehaviour
 {
@@ -8,11 +9,15 @@ public class TutorialController : MonoBehaviour
     private const string tutorialContent = "TutorialContent";
     private const string tutorialTitle = "TutorialTitle";
     private const string tutorialIcon = "TutorialIcon";
+    private const string tutorialMessage = "TutorialMessage";
+    private const string tutorialInside = "TutorialInside";
     
     private VisualElement _tutorialContainer;
     private VisualElement _tutorialContent;
     private Label _tutorialTitle;
     private VisualElement _tutorialIcon;
+    private Label _tutorialMessage;
+    private VisualElement _tutorialInside;
 
     void Awake()
     {
@@ -21,6 +26,8 @@ public class TutorialController : MonoBehaviour
         _tutorialContent = root.Q<VisualElement>(tutorialContent);
         _tutorialTitle = root.Q<Label>(tutorialTitle);
         _tutorialIcon = root.Q<VisualElement>(tutorialIcon);
+        _tutorialMessage = root.Q<Label>(tutorialMessage);
+        _tutorialInside = root.Q<VisualElement>(tutorialInside);
         
         _tutorialContainer.style.display = DisplayStyle.None; // Hide tutorial
     }
@@ -29,24 +36,33 @@ public class TutorialController : MonoBehaviour
     {
         if (Keyboard.current.digit1Key.isPressed)
         {
+            Debug.Log("Pressed digit1");
             // Show Tutorial 1
-            
+            var root = GetComponent<UIDocument>().rootVisualElement;
+            root.SetEnabled(true);
+            _tutorialMessage.text = string.Empty;
             _tutorialContainer.style.display = DisplayStyle.Flex;
             
             // Start animation
-            Invoke("AnimateTutorial", 1f);
+            Invoke("AnimateTutorial", 0.1f);
+        }
+        else if (Keyboard.current.digit0Key.isPressed)
+        {
+            // End animation
+            Invoke("AnimateHideTutorial", 0.1f);
         }
     }
     
     private void OnEnable()
     {
         _tutorialContainer.RegisterCallback<TransitionEndEvent>(OnTutorialShown);
-
+        _tutorialContent.RegisterCallback<TransitionEndEvent>(OnContentShown);
     }
     
     private void OnDisable()
     {
         _tutorialContainer.UnregisterCallback<TransitionEndEvent>(OnTutorialShown);
+        _tutorialContent.UnregisterCallback<TransitionEndEvent>(OnContentShown);
     }
     
     private void AnimateTutorial()
@@ -56,27 +72,88 @@ public class TutorialController : MonoBehaviour
     
     private void OnTutorialShown(TransitionEndEvent evn)
     {
-        if (_tutorialContainer.ClassListContains("tutorial-show") && !_tutorialContent.ClassListContains("content-full"))
+        if (_tutorialContainer.ClassListContains("tutorial-show") && !_tutorialContent.ClassListContains("content-full") && !_tutorialContent.ClassListContains("content-hide"))
         {
             _tutorialContent.AddToClassList("content-full");
             _tutorialTitle.AddToClassList("title-show");
-            _tutorialIcon.AddToClassList("icon-show");
+            //_tutorialIcon.AddToClassList("icon-show");
             
             _tutorialIcon.schedule.Execute(() =>
             {
                 _tutorialIcon.ToggleInClassList("icon-rotate-left");
             });
             
+            Invoke("ActivateTextAndIcon", 3f);
+            /*
+            _tutorialMessage.text = string.Empty;
+            string m = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+            DOTween.To(()=> _tutorialMessage.text, x => _tutorialMessage.text = x, m, 3f).SetEase(Ease.Linear);
+            */
             
             _tutorialIcon.RegisterCallback<TransitionEndEvent>(
                 evt =>
                 {
-                    Debug.Log("ROTATE END");
-                    Debug.Log(_tutorialIcon.ClassListContains("icon-rotate-left"));
                     _tutorialIcon.ToggleInClassList("icon-rotate-left");
                 });
             
+        } 
+        else if (_tutorialContainer.ClassListContains("tutorial-hide"))
+        {
+            Invoke("DisableTutorial", 5f);
+            
         }
+       
+    }
+
+    private void DisableTutorial()
+    {
+        _tutorialContainer.style.display = DisplayStyle.None;
+        _tutorialContainer.RemoveFromClassList("tutorial-hide");
+        _tutorialContainer.RemoveFromClassList("tutorial-show");
+        
+        _tutorialTitle.RemoveFromClassList("title-show");
+        
+        _tutorialContent.RemoveFromClassList("content-hide");
+        _tutorialContent.RemoveFromClassList("content-full");
+        
+        _tutorialIcon.RemoveFromClassList("icon-rotate-left");
+        _tutorialIcon.RemoveFromClassList("icon-show");
+        
+        _tutorialInside.RemoveFromClassList("tutorial-inside-hide");
+
+    }
+
+    private void OnContentShown(TransitionEndEvent evn)
+    {
+        if (_tutorialContent.ClassListContains("content-hide"))
+        {
+            Invoke("HideTutorial", 1.5f);
+        }
+        
+    }
+
+    private void ActivateTextAndIcon()
+    {
+        
+        
+        _tutorialMessage.text = string.Empty;
+        string m = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        DOTween.To(()=> _tutorialMessage.text, x => _tutorialMessage.text = x, m, 6f).SetEase(Ease.Linear)
+            .OnComplete(() => {
+            _tutorialIcon.AddToClassList("icon-show");
+        });;
+    }
+    
+    private void AnimateHideTutorial()
+    {
+        _tutorialContent.AddToClassList("content-hide");
+        _tutorialInside.AddToClassList("tutorial-inside-hide");
+    }
+
+    private void HideTutorial()
+    {
+        
+        _tutorialContainer.AddToClassList("tutorial-hide");
     }
 
 }
