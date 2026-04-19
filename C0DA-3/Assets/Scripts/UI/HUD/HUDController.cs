6d01ab520f75163bea2c58c8560be1fb05b7f43e
aike <aikesiero@gmail.com>
2026-04-19 05:48:39 +0200

    Screw counter and Health animation

--- C0DA-3/Assets/Scripts/UI/HUD/HUDController.cs
+++ C0DA-3/Assets/Scripts/UI/HUD/HUDController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Collections.Generic;
@@ -7,13 +8,24 @@ public class HUDController : MonoBehaviour
 {
     private const string gear = "Gear";
     private const string life = "LifeUnit";
-    
+    private const string healthBar = "ScrewHealingProgress";
+    private const string screwCounter = "ScrewText";
 
     private VisualElement _gear;
     private List<VisualElement> _lifeUnits;
+    private Label _screwText;
+    private ProgressBar _healthBar;
+    private Label _screwCounter;
     
     private float actualAngle = 0f;
     private const float degreesPerTime = 180f;
+
+    private int lastScrewValue = 0;
+    private bool isModifyingScrewValue = false;
+    
+    private AudioSource healSound;
+    private AudioSource damageSound;
+    private AudioSource chargingSound;
     
     // Singleton
     private static HUDController _instance;
@@ -32,42 +44,55 @@ public class HUDController : MonoBehaviour
         
         var root = GetComponent<UIDocument>().rootVisualElement;
         _gear = root.Q<VisualElement>(gear);
+        _healthBar = root.Q<ProgressBar>(healthBar);
+        _screwCounter = root.Q<Label>(screwCounter);
         
         foreach (int i in Enumerable.Range(1, 10))
         {
-            string elementName = "LifeUnit" + i;
+            string elementName = life + i;
             _lifeUnits.Add(root.Q<VisualElement>(elementName));
         }
         
         root.style.display = DisplayStyle.Flex; // Show HUD
         
     }
-    
+
+    void Start()
+    {
+        AudioSource[] sources = GetComponents<AudioSource>();
+        healSound = sources[0];
+        damageSound = sources[1];
+        chargingSound = sources[2];
+    }
     #region Static Methods
     public static void GainLife(int newLife)
     {
         _instance.UpdateLife(newLife);
         _instance.AddLife();
-
     }
     
     public static void LoseLife(int newLife)
     {
         _instance.UpdateLife(newLife);
         _instance.SubstractLife();
-
+    }
+    
+    public static void UpdateHealingCounter(int newValue)
+    {
+        _instance.UpdateHealing(newValue);
     }
     #endregion
 
     private void AddLife()
     {
+        healSound.Play();
         actualAngle += degreesPerTime;
         _gear.style.rotate = new StyleRotate(new Rotate(Angle.Degrees(actualAngle)));
-
     }
     
     private void SubstractLife()
     {
+        damageSound.Play();
         actualAngle -= degreesPerTime;
         _gear.style.rotate = new StyleRotate(new Rotate(Angle.Degrees(actualAngle)));
     }
@@ -86,5 +111,47 @@ public class HUDController : MonoBehaviour
             }
             
         }
+    }
+    
+    private void UpdateHealing(int newValue)
+    {
+        if (!chargingSound.isPlaying && newValue > 0)
+        {
+            chargingSound.Play();
+        }
+        else if (chargingSound.isPlaying && newValue == 0)
+        {
+            chargingSound.Stop();
+        }
+        
+        _healthBar.value = newValue;
+
+        isModifyingScrewValue = (newValue != 0);
+
+        // TODO: Cambiar HUDExmple por el que contentga la info
+        int actualCounter = HUDExample.screwCounter;
+        int provisionalValue = actualCounter - (newValue/10);
+        _screwCounter.text = provisionalValue.ToString();
+
+        if (newValue == 100 && HUDExample.actualLife < HUDExample.initialLife)
+        {
+            HUDExample.screwCounter = provisionalValue;
+            HUDExample.actualLife += 1;
+            chargingSound.Stop();
+            UpdateLife(HUDExample.actualLife);
+            AddLife();
+            _healthBar.value = 0;
+        }
+    }
+
+    private void Update()
+    {
+        int actualCounter = HUDExample.screwCounter;
+        if (lastScrewValue != actualCounter && !isModifyingScrewValue)
+        {
+            _screwCounter.text = actualCounter.ToString();
+            lastScrewValue =  actualCounter;
+        }
+        
     }
 }
--- C0DA-3/Assets/Scripts/UI/HUD/HUDExample.cs
+++ C0DA-3/Assets/Scripts/UI/HUD/HUDExample.cs
@@ -3,10 +3,19 @@ using UnityEngine.InputSystem;
 
 public class HUDExample : MonoBehaviour
 {
-    private int initialLife = 10;
+    public static int initialLife = 10;
+    public static int actualLife;
+    
+    public static int screwCounter = 127;
 
-    private int actualLife;
-
+    private float pressStartTime;
+    private float totalDuration = 0f;
+    private float timeToHealth = 1.5f;
+    private bool isKeyHeld = false;
+    
+    float healingCooldown = 5f;
+    float remainingTime;
+    
     void Awake()
     {
         actualLife = initialLife;
@@ -29,6 +38,66 @@ public class HUDExample : MonoBehaviour
                 actualLife -= 1;
                 HUDController.LoseLife(actualLife);
             }
+        }
+        else if (Keyboard.current.hKey.wasPressedThisFrame)
+        {
+            if (remainingTime <= 0)
+            {
+
+                    pressStartTime = Time.time - totalDuration;
+
+                // totalDuration = 0;
+
+                isKeyHeld = true;
+            }
+        }
+        
+        // Esto se ejecuta CADA frame mientras la tecla esté bajada
+        if (Keyboard.current.hKey.isPressed)
+        {
+            if (isKeyHeld)
+            {
+                totalDuration = Time.time - pressStartTime;
+                if (totalDuration > timeToHealth)
+                {
+                    totalDuration = timeToHealth;
+                }
+            }
+        }
+
+        // Detectar el momento en que se suelta
+        if (Keyboard.current.hKey.wasReleasedThisFrame)
+        {
+            isKeyHeld = false;
+        }
+        
+        
+        // Mientras haya tiempo en el contador informamos
+        if (totalDuration >= 0 && totalDuration <= timeToHealth)
+        {
+            if (remainingTime <= 0)
+            {
+                float percentageComplete = (totalDuration * 100) / timeToHealth;
+                totalDuration -= Time.deltaTime;
+                // si alcanza el maximo activamos el cooldown
+                if ((int)percentageComplete >= 100 && actualLife < initialLife)
+                {
+                    remainingTime = healingCooldown;
+                    totalDuration = 0;
+                }
+                
+                HUDController.UpdateHealingCounter((int)percentageComplete);
+
+                
+            }
+        }
+        
+        
+        // controlamos el cooldown
+        if (remainingTime > 0)
+        {
+            isKeyHeld = false;
+            remainingTime -= Time.deltaTime;
         }
     }
 }
